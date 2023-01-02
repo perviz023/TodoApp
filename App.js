01e3c4a777a5524b3862dd09f3d799da0ac92494
perviz023 <90148295+perviz023@users.noreply.github.com>
2023-01-02 14:40:53 +0400

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
 import React from 'react'
-import { Text, Image, View} from 'react-native'
+import { Text, Image, View, StyleSheet, TouchableOpacity, SafeAreaView, ScrollView, Dimensions} from 'react-native'
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 
 export default function APp() {
   return (
-    <View>
-      <Text>Home</Text>
+    <SafeAreaView>
+      <ScrollView>
+      <View style={styles.container}>
     <Image source={require('./assets/img/Azalia.png')} />
+    <View
+    style={styles.addContainer}
+    >
+    <TouchableOpacity
+    style={styles.button}
+    >
+      <Text
+      style={styles.btnText}
+      >+</Text>
+    </TouchableOpacity>
     </View>
+    </View>
+      </ScrollView>
+    </SafeAreaView>
+    
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: height,
+    width: width,
+  },
+  addContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    width: width-50,
+    height: height-100,
+  },
+  button: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'lightblue',
+    width: 60,
+    height: 50,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  btnText: {
+    fontSize: 30,
+    color: 'white',
+  },
+});
 
