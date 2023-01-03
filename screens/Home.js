7f56df5a67570ff7cb16b357dd19976c285e1f53
perviz023 <90148295+perviz023@users.noreply.github.com>
2023-01-03 14:55:09 +0400

    Update All

--- screens/Home.js
+++ screens/Home.js
@@ -14,7 +14,7 @@ import {
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 
-export default function APp() {
+export default function Home() {
   return (
     <SafeAreaView>
       <ScrollView>
